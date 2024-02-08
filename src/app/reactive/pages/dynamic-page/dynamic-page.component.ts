a1659740d0bc48087e4f0a8515b685067c13454b
Diego La Faye <lafayediego@gmail.com.com>
2024-02-08 18:55:13 -0400

    added formArrays dynamically

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name:['',Validators.required, Validators.minLength(3)],
+    favoriteGames: this.fb.array([
+        ['Darkest Dungeon',Validators.required],
+        ['Detroit become human',Validators.required],
+        ['Left 4 dead',Validators.required],
+      ],
+      )
+  })
+
+
+
+  constructor(private fb:FormBuilder){}
+
+  get FavoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+
+  onSubmit(){
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+
+    this.myForm.reset();
+  }
 }
