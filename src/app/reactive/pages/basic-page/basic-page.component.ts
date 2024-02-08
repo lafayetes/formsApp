4eaef712f4ae12843600427e6f27ef0e14604076
Diego La Faye <lafayediego@gmail.com.com>
2024-02-08 13:22:00 -0400

    added validators and first approach to how handle the errors messages from validations

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+  // Forma basica de hacer formgroups
+  // public myForm:FormGroup = new FormGroup({
+  //   name : new FormControl(''),
+  //   price : new FormControl(0),
+  //   inStorage : new FormControl(0),
+  // })
 
+
+  //Forma recomendade de hacer forms con el formbuilder
+  //name:['',[] ,[]] , Esta es la manera donde se coloca en el formbuild, el primero es el valor, el segundo las validaciones sincronas y el tercero las funciones asingronas
+
+  public myForm:FormGroup = this.fb.group({
+    name:['',[Validators.required,Validators.minLength(3)]],
+    price:[0,[Validators.required, Validators.min(0)]],
+    inStorage:[0,[Validators.required, Validators.min(0)]],
+  })
+
+  onSave(){
+
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    this.myForm.reset({price:10,inStorage:0})
+    console.log(this.myForm.value);
+
+  }
+  constructor(private fb:FormBuilder){}
 }
